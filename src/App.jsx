a4047f5596701207fe0c209d07a1ddfb3be53c21
kaziabulhasib <hasibkazi420@gmail.com>
2025-01-26 22:04:45 +0530

    basic file setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className=' text-2xl text-red-500 border border-green-100 text-center'>
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
