bfeface6b63e1e3df0ebb871517d6e3a5944777b
kaziabulhasib <hasibkazi420@gmail.com>
2025-01-28 12:20:57 +0530

    show visited countries in the top

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries }) => {
   const { cca3, name, flags } = country;
   const [visited, setVisited] = useState(false);
   const handleVisited = () => {
     setVisited(!visited);
   };
   return (
-    <div className='p-4 border border-slate-400'>
+    <div
+      className={`p-4 border border-slate-400 m-6 ${
+        visited ? "bg-amber-100" : "bg-gray-100"
+      } `}>
       <h1 className='text-xl font-semibold my-6'>
         Common Name : {name?.common}
       </h1>
       <img src={flags.png} alt='flags' />
       <h4 className='my-4'>Code: {cca3}</h4>
       <button
+        className='px-4 py-2 rounded-sm bg-amber-600 my-4 cursor-pointer'
+        onClick={() => handleVisitedCountries(country)}>
+        Mark as Visited
+      </button>{" "}
+      <br />
+      <button
         onClick={handleVisited}
         className=' px-4 py-2 bg-red-300 hover:bg-red-400 rounded-sm cursor-pointer'>
-        Visited
+        {visited ? "visited" : "going"}
       </button>
       <h1
         className={`my-4 ${
--- src/components/countries.jsx
+++ src/components/countries.jsx
@@ -4,6 +4,7 @@ import Country from "./Country";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedCountries, setVisistedCountries] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
@@ -12,12 +13,31 @@ const Countries = () => {
         setCountries(data);
       });
   }, []);
+  const handleVisitedCountries = (country) => {
+    console.log(country.name.common);
+    const newVisitedCountry = [...visitedCountries, country];
+    setVisistedCountries(newVisitedCountry);
+  };
   return (
     <div>
       <h1 className='text-3xl font-bold text-center my-6'>Countries</h1>
-      <div className='grid grid-cols-3'>
+      <div>
+        <h1 className='text-xl text-center font-semibold'>
+          Visited Countries: {visitedCountries.length}
+        </h1>
+        <ul className='flex flex-col justify-center items-center'>
+          {visitedCountries.map((country) => (
+            <li key={country.cca3}>{country.name.common}</li>
+          ))}
+        </ul>
+      </div>
+      <div className='grid grid-cols-4'>
         {countries.map((country) => (
-          <Country country={country} key={country.cca3} />
+          <Country
+            key={country.cca3}
+            country={country}
+            handleVisitedCountries={handleVisitedCountries}
+          />
         ))}
       </div>
     </div>
