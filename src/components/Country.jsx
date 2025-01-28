dda1128c5308daa0ec3736fc6505177c60567438
kaziabulhasib <hasibkazi420@gmail.com>
2025-01-28 11:04:41 +0530

    toggle button & conditionl rendering

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -4,19 +4,28 @@ const Country = ({ country }) => {
   const { cca3, name, flags } = country;
   const [visited, setVisited] = useState(false);
   const handleVisited = () => {
-    setVisited(true);
+    setVisited(!visited);
   };
   return (
     <div className='p-4 border border-slate-400'>
-      <h1 className='text-xl font-medium my-6'>Common Name : {name?.common}</h1>
+      <h1 className='text-xl font-semibold my-6'>
+        Common Name : {name?.common}
+      </h1>
       <img src={flags.png} alt='flags' />
       <h4 className='my-4'>Code: {cca3}</h4>
       <button
         onClick={handleVisited}
-        className=' px-4 py-2 bg-red-300 rounded-sm cursor-pointer'>
+        className=' px-4 py-2 bg-red-300 hover:bg-red-400 rounded-sm cursor-pointer'>
         Visited
       </button>
-      <h1 className='my-4'>{visited && "I have visited this country"}</h1>
+      <h1
+        className={`my-4 ${
+          visited ? "text-red-500" : "text-green-600"
+        } text-lg font-medium`}>
+        {visited
+          ? "I have visited this country"
+          : "I want to visit this country"}
+      </h1>
     </div>
   );
 };
